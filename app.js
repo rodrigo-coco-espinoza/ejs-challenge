1b1ee334a000777127f9e87f694f52fc3eb4274f
Coco <respinozafuentes@gmail.com>
2023-02-25 10:27:30 -0300

    Complete Challenge 6, 7 and 8

--- app.js
+++ app.js
@@ -20,9 +20,6 @@ app.get( "/", function( req, res ) {
   res.render("home", { startingContent: homeStartingContent });
 });
 
-
-
-
 app.get( "/about", function( req, res ) {
   res.render( "about", { aboutContent: aboutContent });
 });
@@ -31,7 +28,14 @@ app.get( "/contact", function( req, res ) {
   res.render( "contact", { contactContent: aboutContent });
 });
 
+app.get( "/compose", function( req, res ) {
+  res.render( "compose" );
+});
 
+app.post( "/compose", function( req, res ) {
+  text = req.body.newText;
+  console.log(text);
+});
 
 
 
